f5f7bc2397697a3227747675717cafa57d802f4c
MinJiwon <jwjwmin111@gmail.com>
2022-04-28 17:30:14 +0900

    객체로 데이터 받아와서 추천스팟 상세페이지에 나타내는 기능 추가

--- js/detail.js
+++ js/detail.js
@@ -1,24 +1,60 @@
-let firstImg = document.querySelector(".first-img");
+let recomSpots = [
+  {
+    title: "성산근린공원",
+    title_des: "성미산 한바퀴",
+    slide_img: ["sungsan_park.jpg", "sungsan_park2.jpg"],
+    description: `성산근린공원 짱짱맨`,
+  },
+  {
+    title: "먼스스컬프쳐",
+    title_des: "영감이 필요할 때",
+    slide_img: ["month_sculpture.jpg"],
+    description: `먼스스컬프쳐 짱짱맨`,
+  },
+];
 
-let slideImg = ["sungsan_park.jpg", "sungsan_park2.jpg"];
-let detailImg = document.querySelector(".detail-img");
-let bgImg = document.querySelector(".bg-img");
-let imgPath = "../images/";
-
-let detail = document.querySelector(".detail");
-let closeBtn = document.querySelector(".close-btn");
-
+const detail = document.querySelector(".detail-wrapper");
+let slideImg = [];
 let idx = 0;
+const closeBtn = document.querySelector(".close-btn");
 
-firstImg.addEventListener("click", () => {
-  detail.style.display = "block";
-});
+// 추천스팟 클릭이벤트 핸들러
+function showDetailHandler(e) {
+  let name = e.target.alt;
+  let result = {};
 
-function plusSlides(n) {
-  showSlides((idx += n));
+  recomSpots.forEach((elm) => {
+    if (name == elm.title) {
+      result = elm;
+    }
+  });
+
+  showModal(result);
 }
 
+// 모달창 띄우는 함수
+function showModal(res) {
+  detail.style.display = "block";
+
+  const title1 = document.querySelector("#title1");
+  title1.innerText = res.title_des;
+
+  const title2 = document.querySelector("#title2");
+  title2.innerText = res.title;
+
+  slideImg = res.slide_img;
+  showSlides(idx, slideImg);
+
+  const detailInfo = document.querySelector("#detail-info");
+  detailInfo.innerText = res.description;
+}
+
+// 슬라이드 쇼 띄우는 함수
 function showSlides(n) {
+  const bgImg = document.querySelector(".bg-img");
+  const imgPath = "../images/";
+  const detailImg = document.querySelector(".detail-img");
+
   if (n < 0) {
     idx = slideImg.length - 1;
   }
@@ -31,8 +67,12 @@ function showSlides(n) {
   bgImg.style.backgroundImage = `url(${imgPath}${slideImg[idx]})`;
 }
 
-showSlides(idx);
+// 슬라이드 쇼 넘기는 함수
+function plusSlides(n) {
+  showSlides((idx += n));
+}
 
+// 닫기 버튼 기능
 closeBtn.addEventListener("click", () => {
   detail.style.display = "none";
 });
