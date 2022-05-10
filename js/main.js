34d3f4529c1f51eaf454763c697213be430940d0
MinJiwon <jwjwmin111@gmail.com>
2022-05-10 17:37:17 +0900

    헤더에 발자국 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ document.addEventListener("scroll", function () {
     nav.style.backgroundColor = "#019267";
   }
 });
+
+const shoePrintLeft = document.getElementById("shoe-print-left");
+const shoePrintLeft2 = document.getElementById("shoe-print-left2");
+const shoePrintRight = document.getElementById("shoe-print-right");
+const shoePrintRight2 = document.getElementById("shoe-print-right2");
+
+// setInterval(() => {
+setTimeout(() => {
+  shoePrintLeft.style.display = "block";
+}, 1000);
+setTimeout(() => {
+  shoePrintRight.style.display = "block";
+}, 2000);
+setTimeout(() => {
+  shoePrintLeft2.style.display = "block";
+}, 3000);
+setTimeout(() => {
+  shoePrintRight2.style.display = "block";
+}, 4000);
+shoePrintLeft.style.display = "none";
+shoePrintLeft2.style.display = "none";
+shoePrintRight.style.display = "none";
+shoePrintRight2.style.display = "none";
+// }, 5000);
